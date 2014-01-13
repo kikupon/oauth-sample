7b5cce696c97ee2c36da6fc95df9df1f678332dd
nab <napu1031@gmail.com>
2014-01-13 14:49:47 +0900

    add twitter login

--- app/TapController.rb
+++ app/TapController.rb
@@ -14,6 +14,7 @@ class TapController < UIViewController
     right_button = UIBarButtonItem.alloc.initWithTitle("Push", style: UIBarButtonItemStyleBordered, target:self, action:'push')
     self.navigationItem.rightBarButtonItem = right_button
 
+=begin
     @account_store = ACAccountStore.alloc.init
     @account_type = @account_store.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierFacebook);
     @options = {ACFacebookAppIdKey       => '654511364587200',
@@ -24,6 +25,25 @@ class TapController < UIViewController
                                                                                                        accounts = @account_store.accountsWithAccountType(@account_type)
                                                                                                        anAccount = accounts.lastObject
                                                                                                        NSLog(anAccount("#{anAccount.description}"))
+                                                                                                   else
+                                                                                                       NSLog("error: #{error.description}")
+                                                                                                   end
+                                                                                                 end
+                                                 )
+=end
+    @account_store = ACAccountStore.alloc.init
+    @account_type = @account_store.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter);
+    @options = {ACFacebookAppIdKey       => '654511364587200',
+                ACFacebookPermissionsKey => ['email'],
+               }
+    @options = nil
+    @account_store.requestAccessToAccountsWithType(@account_type, options: @options, completion: lambda do |granted, error|
+                                                                                                   if granted
+                                                                                                       accounts = @account_store.accountsWithAccountType(@account_type)
+                                                                                                       anAccount = accounts.lastObject
+                                                                                                       user_id = anAccount.valueForKeyPath("properties.user_id")
+                                                                                                       NSLog("#{user_id}")
+                                                                                                       NSLog("#{anAccount.username}")
                                                                                                    else
                                                                                                        NSLog("error: #{error.description}")
                                                                                                    end
